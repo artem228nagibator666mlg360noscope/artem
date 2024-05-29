6f0611bfbe989c8243be0620916d42ee5a2a275f
artem228nagibator666mlg360noscope <az09artemderbaskin@gmail.com>
2024-05-29 16:32:43 +0500

    js dz

---
+++ ДЗ 29.05.24/7/js/script.js
@@ -0,0 +1,3 @@
+for (var i = 10; i <= 20; i++) {
+    document.write((i * i) + "<br>");
+}
---
+++ ДЗ 29.05.24/8/js/script.js
@@ -0,0 +1,5 @@
+var number1 = parseInt(prompt("Введите число 1: "));
+
+var number2 = parseInt(prompt("Введите число 2: "));
+var res = number1 + number2;
+document.write("<h1>"+ res + "</h1>");
