728faecf3225f5d4624dd4a23383902f4f0f4db3
artem228nagibator666mlg360noscope <az09artemderbaskin@gmail.com>
2024-06-03 00:27:54 +0500

    password

---
+++ ДЗ 02.06.24/js/script.js
@@ -0,0 +1,32 @@
+var div_res = document.getElementById("res");
+
+function check_username() {
+    var password = form.password.value;
+    var symbols_in_password = password.length;
+
+    div_res.innerHTML = "";
+
+    if (password == ""){
+        div_res.innerHTML = "Пароль не должен быть пустым!";
+    }
+
+    if(symbols_in_password <= 6){
+        div_res.innerHTML += "<br>Пароль должен быть больше шести символов";
+    }
+
+    if (!hasUppercase(password)) {
+        div_res.innerHTML += "<br>Пароль должен содержать хотя бы одну заглавную букву";
+    }
+
+    is_number_in_string(password);
+}
+
+function hasUppercase(password) {
+    for (var i = 0; i < password.length; i++) {
+        var ch = password.charAt(i);
+        if (ch === ch.toUpperCase() && ch !== ch.toLowerCase()) {
+            return true;
+        }
+    }
+    return false;
+}
