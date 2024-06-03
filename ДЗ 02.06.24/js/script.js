2d15ad324f25b1885a581d5c6acc4f13232bb34c
artem228nagibator666mlg360noscope <az09artemderbaskin@gmail.com>
2024-06-03 16:20:04 +0500

    исправил

--- ДЗ 02.06.24/js/script.js
+++ ДЗ 02.06.24/js/script.js
@@ -1,19 +1,24 @@
 var div_res = document.getElementById("res");
 
-function check_username() {
+function check_username_and_password() {
+    var username = form.username.value;
     var password = form.password.value;
     var symbols_in_password = password.length;
 
     div_res.innerHTML = "";
 
-    if (password == ""){
-        div_res.innerHTML = "Пароль не должен быть пустым!";
+    if (username == "") {
+        div_res.innerHTML = "Имя не должно быть пустым";
     }
 
-    if(symbols_in_password <= 6){
+    if (password == "") {
+        div_res.innerHTML += "<br>Пароль не должен быть пустым";
+    } 
+    
+    if (symbols_in_password <= 6) {
         div_res.innerHTML += "<br>Пароль должен быть больше шести символов";
-    }
-
+    } 
+    
     if (!hasUppercase(password)) {
         div_res.innerHTML += "<br>Пароль должен содержать хотя бы одну заглавную букву";
     }
