c6b752290dbd772b04888398afbd558d75e4d2a5
artem228nagibator666mlg360noscope <az09artemderbaskin@gmail.com>
2024-06-05 16:31:14 +0500

    н

--- ДЗ 05.06.24/js/script.js
+++ ДЗ 05.06.24/js/script.js
@@ -4,34 +4,33 @@ function check_username_and_password() {
     var username = form.username.value;
     var password = form.password.value;
     var symbols_in_password = password.length;
-
-    div_res.innerHTML = "";
+    var s = ""
 
     if (username == "") {
-        div_res.innerHTML = "Имя не должно быть пустым";
+        s += "Имя не должно быть пустым";
     }
 
     if (password == "") {
-        div_res.innerHTML += "<br>Пароль не должен быть пустым";
-    } 
-    
+        s += "Пароль не должен быть пустым";
+    }
+
     if (symbols_in_password <= 6) {
-        div_res.innerHTML += "<br>Пароль должен быть больше шести символов";
-    } 
-    
+        s += "Пароль должен быть больше шести символов";
+    }
+
     if (!hasUppercase(password)) {
-        div_res.innerHTML += "<br>Пароль должен содержать хотя бы одну заглавную букву";
+        s += "Пароль должен содержать хотя бы одну заглавную букву";
     }
 
     is_number_in_string(password);
 
-    
+    div_res.innerHTML
 }
 
 function hasUppercase(password) {
     for (var i = 0; i < password.length; i++) {
         var ch = password.charAt(i);
-        if (ch === ch.toUpperCase() && ch !== ch.toLowerCase()) {
+        if (ch == ch.toUpperCase() && ch != ch.toLowerCase()) {
             return true;
         }
     }
