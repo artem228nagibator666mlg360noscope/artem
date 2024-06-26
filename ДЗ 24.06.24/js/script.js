5d705860d4f66ad6630960d4bbc523214279b47f
artem228nagibator666mlg360noscope <az09artemderbaskin@gmail.com>
2024-06-26 15:10:49 +0500

    доделал

--- ДЗ 24.06.24/js/script.js
+++ ДЗ 24.06.24/js/script.js
@@ -1,16 +1,38 @@
 var button = document.getElementById("hamburger");
-var a = document.getElementsByClassName("nav");
+var a = document.getElementById("menu_desktop");
 
-for (let i = 0; i < a.length; i++) {
-    a[i].style.display = "block";         
-}
+
 
 button.addEventListener("click", function(){
     for (let i = 0; i < a.length; i++) {
-        if (a[i].style.display == "block"){
-            a[i].style.display = "none";
-        }else{
+        if (a[i].style.display == "none"){
             a[i].style.display = "block";
+        }else{
+            a[i].style.display = "none";
         }
     }
 });
+
+var button = document.getElementById("hamburger");
+var menuMobile = document.getElementById("menu_mobile");
+
+button.addEventListener("click", function(){
+    if (menuMobile.style.display === "none") {
+        menuMobile.style.display = "block";
+    } else {
+        menuMobile.style.display = "none";
+    }
+});
+
+function closeMobileMenu() {
+    menuMobile.style.display = "none";
+}
+
+function handleResize() {
+    if (window.innerWidth >= 1200) {
+        closeMobileMenu();
+    }
+}
+
+window.addEventListener('resize', handleResize);
+handleResize();
